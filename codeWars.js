c72ea4cc279d1592225e716c7ec05e3095582582
Peter Opara <oparap8@gmail.com>
2024-11-13 14:22:27 +0200

    first commit

---
+++ codeWars.js
@@ -0,0 +1,13 @@
+//Sort the odd
+function sortArray(array) {
+    const odd = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+    let oddIndex = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 !== 0) {
+            array[i] = odd[oddIndex];
+            oddIndex++;
+        }
+    }
+    return array;
+}
+
