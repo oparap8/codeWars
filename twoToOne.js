895dabf3b2c305adfcf5fc3220c1240e40296e2b
Peter Opara <oparap8@gmail.com>
2024-11-18 10:37:39 +0200

    18/11/24

---
+++ twoToOne.js
@@ -0,0 +1,11 @@
+function longest(s1, s2) {
+    let s3 = s1 + s2;
+    let myArray = [];
+    for (let i = 0; i < s3.length; i++){
+      if (myArray.includes(s3[i])){
+        continue
+      }else{
+        myArray.push(s3[i])
+      }
+    }
+    return myArray.sort().join('')
